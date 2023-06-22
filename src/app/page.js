6a7383ab4a3269d8a112fec66a5045eeab18cff1
Renchie <ryan0042@student.monash.edu>
2023-06-22 20:44:00 +1000

    poopoo

--- src/app/page.js
+++ src/app/page.js
@@ -6,25 +6,26 @@ import image from '../images/hero image.png'
 
 export default function Home() {
   return (
-    <>
-      <div className='home-body'>
-        <div className='home-body-inner'>
-          <div className='hero-content'>
-            <div className='hero-content-title'>life with lam</div>
-            <div className='hero-content-text'>
-              <p>for those struggling to find their why</p>
-              <p>a passion project that seeks to uncover greater perspective</p>
-              <p>an individual looking to defy the modern day career trajectory!</p>
-            </div>
-          </div>
-          <div className='hero-image-container'>
-            <Image fill objectFit='contain' src={image} placeholder='blur'
-              sizes='(max-width: 640px) 90vw, 33vw'
-            />
+    <div className='home-body'>
+
+      <div className='home-body-inner-container'>
+
+        <div className='hero-content flex-element'>
+          <div className='hero-content-title'>life with lam</div>
+          <div className='hero-content-text'>
+            <p>for those struggling to find their why</p>
+            <p>a passion project that seeks to uncover greater perspective</p>
+            <p>an individual looking to defy the modern day career trajectory!</p>
           </div>
         </div>
+        <div className='hero-image-container flex-element'>
+          <Image fill className='hero-image' src={image} placeholder='blur'
+            sizes='(max-width: 640px) 90vw, 33vw'
+          />
+        </div>
       </div>
-    </>
+
+    </div>
   )
 }
 
